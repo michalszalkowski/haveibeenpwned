3a681b17735a4eca843c8d83d79dac0103a96e3e
szalek <x>
2017-11-10 02:30:28 +0100

    fix typo

--- haveibeenpwned.sh
+++ haveibeenpwned.sh
@@ -9,20 +9,20 @@ GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 NC='\033[0m'
 
-while read url;
+while read email;
     do
 
-        statusCode=$(curl --write-out %{http_code} --silent --output /dev/null "https://haveibeenpwned.com/api/v2/breachedaccount/$url")
+        statusCode=$(curl --write-out %{http_code} --silent --output /dev/null "https://haveibeenpwned.com/api/v2/breachedaccount/$email")
 
         if [ "$statusCode" == 200 ]
             then
-                echo -e ${RED} 'Oh no — pwned!' ${url} ${NC}
+                echo -e ${RED} 'Oh no — pwned!' ${email} ${NC}
             else
         if [ "$statusCode" == 404 ]
             then
-                echo -e ${GREEN} 'Good news — no pwnage found!' ${url} ${NC}
+                echo -e ${GREEN} 'Good news — no pwnage found!' ${email} ${NC}
             else
-                echo -e  ${YELLOW} 'Error' ${url} ${NC}
+                echo -e  ${YELLOW} 'Error' ${email} ${NC}
             fi
         fi
         sleep 2
