f68e600deb7be4608d6cf0c934a543de098b31a2
szalek <x>
2017-11-10 02:55:55 +0100

    extend

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 api form https://haveibeenpwned.com
 
 ./haveibeenpwned.sh emails.txt
+
+./haveibeenpwned.sh test@example.com
+./haveibeenpwned.sh noexist@btbw.pl
--- haveibeenpwned.sh
+++ haveibeenpwned.sh
@@ -1,31 +1,41 @@
 #!/usr/bin/env bash
 
-FILE=$1
+ARG=$1
 
-for FILE1 in "$@"
-do
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
 
-    RED='\033[0;31m'
-    GREEN='\033[0;32m'
-    YELLOW='\033[0;33m'
-    NC='\033[0m'
+function pwned {
 
-    while read email;
-        do
+    statusCode=$(curl --write-out %{http_code} --silent --output /dev/null "https://haveibeenpwned.com/api/v2/breachedaccount/$email")
 
-            statusCode=$(curl --write-out %{http_code} --silent --output /dev/null "https://haveibeenpwned.com/api/v2/breachedaccount/$email")
+    if [ "$statusCode" == 200 ]
+        then
+            echo -e ${RED} 'Oh no — pwned!' ${email} ${NC}
+        else
+    if [ "$statusCode" == 404 ]
+        then
+            echo -e ${GREEN} 'Good news — no pwnage found!' ${email} ${NC}
+        else
+            echo -e  ${YELLOW} 'Error' ${email} ${NC}
+        fi
+    fi
 
-            if [ "$statusCode" == 200 ]
-                then
-                    echo -e ${RED} 'Oh no — pwned!' ${email} ${NC}
-                else
-            if [ "$statusCode" == 404 ]
-                then
-                    echo -e ${GREEN} 'Good news — no pwnage found!' ${email} ${NC}
-                else
-                    echo -e  ${YELLOW} 'Error' ${email} ${NC}
-                fi
-            fi
+}
+
+if [ $ARG == *.txt ]
+  then
+    for FILE in "$@"
+    do
+        while read email;
+            do
+            pwned email
             sleep 2
-    done < ${FILE}
-done
+        done < ${ARG}
+    done
+  else
+    email=$ARG
+    pwned $email
+fi
